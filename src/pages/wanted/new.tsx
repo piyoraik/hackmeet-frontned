2608ca3aa9919c4d182618167c07d99d9be0451b
piyoraik <42093052+piyoraik@users.noreply.github.com>
2022-02-01 16:41:41 +0900

    Syntax当てた

--- src/pages/wanted/new.tsx
+++ src/pages/wanted/new.tsx
@@ -11,15 +11,33 @@ import {
   Textarea,
 } from "@chakra-ui/react";
 import { NextPage } from "next";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { FaCode } from "react-icons/fa";
 import Card from "../../components/atoms/Card";
 import Header from "../../components/organisms/Header";
-import SideNavShow from "../../components/organisms/SideNavShow";
 import { VscDebugStart } from "react-icons/vsc";
+import MarkdownIt from "markdown-it";
+import style from "../../styles/github.module.scss";
+import Prism from "prismjs";
+// import style from "@/style/github.scss";
 
 const NewWanted: NextPage = () => {
   const [text, setText] = useState("");
+
+  const markdownIt = new MarkdownIt({
+    html: true,
+    linkify: true,
+    typographer: true,
+  });
+  const changeHandler = (event: any) => {
+    const res = markdownIt.render(event.target.value!);
+    console.log(res);
+    setText(res);
+  };
+
+  useEffect(() => {
+    Prism.highlightAll();
+  });
 
   return (
     <Box width="80%" mx="auto">
@@ -80,13 +98,14 @@ const NewWanted: NextPage = () => {
                     height="calc(100vh - 50px)"
                     p="10"
                     _focus={{ outline: "none" }}
-                    onChange={(e) => setText(e.target.value)}
+                    onChange={(e) => changeHandler(e)}
                   />
                 </TabPanel>
                 <TabPanel>
-                  <Box p="10" height="calc(100vh - 50px)">
-                    {text}
-                  </Box>
+                  <Box
+                    className={`${style.main} prose`}
+                    dangerouslySetInnerHTML={{ __html: text }}
+                  />
                 </TabPanel>
               </TabPanels>
             </Tabs>
