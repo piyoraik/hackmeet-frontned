8b04834c2da542791264d02b7aee08873b142b87
piyoraik <42093052+piyoraik@users.noreply.github.com>
2022-02-05 23:29:51 +0900

    Update client.ts

--- src/graphql/client.ts
+++ src/graphql/client.ts
@@ -2,6 +2,6 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 import getConfig from "next/config";
 
 export const client = new ApolloClient({
-  uri: `${process.env.BACKEND}/graphql`,
+  uri: `${process.env.NEXT_PUBLIC_BACKEND}/graphql`,
   cache: new InMemoryCache(),
 });
